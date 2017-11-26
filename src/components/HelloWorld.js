ea866185a9706a3fdfd6c01924815d8f8a21671a
stkraft <steffen.kraft@mail.de>
2017-11-26 16:15:51 +0100

    adding styled components and stylelint

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,9 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 10px 30px;
+  margin: 20px 0;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+  cursor: pointer;
+`;
 
 const HelloWorld = () => (
   <div>
     <div>
       <h1>Hello World</h1>
+      <Button>Click me</Button>
     </div>
   </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           "babel-loader",
-          "eslint-loader"
+          "eslint-loader",
+          "stylelint-custom-processor-loader"
         ]
       }
     ]
