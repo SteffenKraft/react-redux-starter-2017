d82217a858c4288e74344a327aac8d6e3fc5093d
stkraft <steffen.kraft@mail.de>
2017-11-27 20:29:05 +0100

    env, bundle.hash, HtmlWebpackPlugin, source-map

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src", "index.js"),
+module.exports = (env = {}) => {
+  const isProduction = env === "production";
 
-  output: {
-    path: path.resolve(__dirname, "output"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          "babel-loader",
-          "eslint-loader",
-          "stylelint-custom-processor-loader"
-        ]
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: "url-loader"
-      }
-    ]
-  },
-  devServer: {
-    contentBase: "./src",
-    publicPath: "/output"
-  }
+  return {
+    context: path.resolve(__dirname, "./src"),
+    entry: {
+      app: "./index.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "[name].bundle.[hash].js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            "babel-loader",
+            "eslint-loader",
+            "stylelint-custom-processor-loader"
+          ]
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: "url-loader"
+        }
+      ]
+    },
+    devtool: isProduction
+      ? "hidden-source-map"
+      : "cheap-module-eval-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+    ],
+  };
 };
